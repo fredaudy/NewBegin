e76ae771311363d74040b12307ab0a4e046d677a
fredaudy <fredaudy@gmail.com>
2020-02-18 01:07:17 +0100

    with tilemap_0

---
+++ background.lua
@@ -0,0 +1,52 @@
+local background = {
+  x = 50, y = 50
+  , tilesMap = {}
+  , tileset = {}
+  , layer = {}
+  , quads = {}
+}
+
+function background:setTilesMap()
+  background.img = love.graphics.newImage("/Assets/ScrollOSprites.png")
+  background.tilesMap = require("/Assets/MyFirstTileMap")
+  background.tileset = background.tilesMap.tilesets[1]
+  background.layer = background.tilesMap.layers[1]
+end
+
+function background:setQuads()
+  for y = 0, background.tileset.imageheight - background.tileset.tileheight, background.tileset.tileheight do
+    for x = 0, background.tileset.imagewidth - background.tileset.tilewidth, background.tileset.tilewidth do
+      table.insert(background.quads, 
+        love.graphics.newQuad(
+          x, 
+          y, 
+          background.tileset.tilewidth, 
+          background.tileset.tileheight, 
+          self.img:getDimensions()))
+    end
+  end
+end
+
+function background:load()
+    background:setTilesMap()
+    background:setQuads()
+end
+
+function background:draw()
+ for y = 0, background.layer.height - 1 do
+      for x = 0, background.layer.width - 1 do
+        if background.layer.data[x + (y * background.layer.width) + 1] == nil then
+          print("ICI")
+        end
+        if background.layer.data[x + (y * background.layer.width) + 1] ~= 0 then          
+          love.graphics.draw(
+            background.img, 
+            background.quads[background.layer.data[x + (y * background.layer.width) + 1]], 
+            x*background.tileset.tilewidth, 
+            y*background.tileset.tileheight)
+        end
+      end
+    end
+end
+
+return background
--- main.lua
+++ main.lua
@@ -1,14 +1,18 @@
 local _player = require("player")
 local player = _player:new()
+local _background = require("background")
 
 function love.load()
-    player:load()
+  if arg[#arg] == "-debug" then require("mobdebug").start() end
+  --player:load()
+  _background:load()
 end
 
 function love.draw()
-    player:draw()
+  --player:draw()
+  _background:draw()
 end
 
 function love.update(dt)
-    player:update(dt)
+    --player:update(dt)
 end
